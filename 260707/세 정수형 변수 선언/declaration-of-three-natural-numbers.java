b73613a081e0bd3173b421d4f52962aa4efa3d84
code_Ha <110831304+Ha-coding-user@users.noreply.github.com>
2026-07-07 14:34:37 +0900

    [Passed] 세 정수형 변수 선언 (103ms, 10MB)

---
+++ 260707/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int num1 = 7;
+        int num2 = 23;
+        int num3 = 30;
+
+        System.out.println(num1 + " + " + num2 + " = " + num3);
+    }
+}
